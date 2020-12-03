ed4c4b64876673cdf9deaf1a4a0b94d7ac06be9b
Vidocq <949867756@qq.com>
2020-12-03 21:52:48 +0800

    2. Vue.js connects to RESTful API by axios

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # 注册 blueprint
+    # enable CORS
+    CORS(app)
+
+    # register blueprint
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
---
+++ front-end/src/components/ping.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/api/ping'
+      axios.get(path)
+        .then(response => {
+          this.msg = response.data
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  },
+  created () {
+    this.getMessage()
+  }
+}
+</script>
